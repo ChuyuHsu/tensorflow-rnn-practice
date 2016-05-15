97ba356056d9eacbeab98fd71c598f1a5ebd6eca
Zoe Tsai <zoetsai@Zoes-MacBook-Pro.local>
2016-05-15 11:02:47 +0800

    complete loss

--- dynamic_rnn_atis.py
+++ dynamic_rnn_atis.py
@@ -5,6 +5,7 @@
 
 import tensorflow as tf
 import numpy as np
+import pdb
 
 #Defining some hyper-params
 n_hidden = 2       #this is the parameter for input_size in the basic LSTM cell
@@ -54,13 +55,8 @@ print(max_sentence_length)
 print(vocsize)
 print(idx_pad)
 
-
-# In[5]:
-
 len(train_lex) + len(test_lex)
 
-
-# In[6]:
 
 def padding(sentence, pad=-1, max_length=50):
     length = len(sentence)
@@ -104,7 +100,7 @@ def gen_data(source, s_pad, Y, y_pad, max_length, vocsize, nclasses, n_batch=5):
             m = csr_matrix((data, (row, col)), shape=(max_length, nclasses))
             y.append(m.toarray())
 
-        yield (seq_lengths, np.array(X), np.array(y))
+        yield (np.array(X), np.array(y), seq_lengths)
 
 
 # In[9]:
@@ -120,36 +116,16 @@ def gen_test_data(n_batch):
             yield i
 
 
-# In[10]:
 
-g = gen_test_data(3)
-
-
-# In[11]:
-
-sl, X, y = next(g)
-
-
-# In[12]:
-
-X.shape
-
-
-# In[13]:
-
-print(y.shape)
-print(y[0].shape)
-
-
-# ### Model Construction
+### Model Construction
 
 # In[14]:
 
 #create placeholders for X and y
 
-input_x = tf.placeholder(tf.int32, shape=[batch_size, max_sentence_length])
-input_y = tf.placeholder(tf.float32, shape=[batch_size, max_sentence_length, nclasses])
-sequence_length = tf.placeholder(tf.int32, shape=[batch_size,])
+input_x = tf.placeholder(tf.int32, shape=[batch_size, max_sentence_length], name="input_x")
+input_y = tf.placeholder(tf.float32, shape=[batch_size, max_sentence_length, nclasses], name="input_y")
+sequence_length = tf.placeholder(tf.int32, shape=[batch_size,], name="sequence_length")
 
 with tf.device('/cpu:0'), tf.name_scope("embedding"):
     W = tf.Variable(
@@ -157,31 +133,53 @@ with tf.device('/cpu:0'), tf.name_scope("embedding"):
         name="W")
     embedded_chars = tf.nn.embedding_lookup(W, input_x) #
 
+print("embedding shape: %s" % str(embedded_chars.get_shape()))
+
+
+# with tf.Session() as sess:
+#     num_units = 10
+#     input_size = embedding_size
+#     cell = tf.nn.rnn_cell.GRUCell(num_units=num_units, input_size=input_size)
+#
+#     with tf.variable_scope("dynamic_scope"):
+#         outputs_dynamic, state_dynamic = tf.nn.dynamic_rnn(
+#             cell, inputs=embedded_chars, sequence_length=sequence_length,
+#             time_major=False, dtype=tf.float32)
+#
+#     X, y, sl = next(gen_train_data(batch_size))
+#     print(X.shape)
+#     feeds = {input_x: X, sequence_length: sl}
+#
+#     # Initialize
+#     tf.initialize_all_variables().run()
+#
+#     outputs_dynamic, _ = sess.run([outputs_dynamic, state_dynamic], feed_dict=feeds)
+#     print(outputs_dynamic)
+# raise
 
 # In[15]:
+with tf.variable_scope("cell"):
+    cell = tf.nn.rnn_cell.BasicLSTMCell(embedding_size)
 
-cell = tf.nn.rnn_cell.BasicLSTMCell(embedding_size)
+# pdb.set_trace()
+with tf.variable_scope("dynamic_scope"):
+    outputs, states = tf.nn.dynamic_rnn(cell, embedded_chars,
+                                        sequence_length=sequence_length,
+                                        time_major=False,
+                                        dtype=tf.float32)
+    print("shape of outputs: %s" % str(outputs.get_shape()))
 
-outputs, states = tf.nn.dynamic_rnn(cell, embedded_chars,
-                                    sequence_length=sequence_length,
-                                    time_major=False,
-                                    dtype=tf.float32)
-print("shape of outputs: %s" % str(tf.shape(outputs)))
+with tf.variable_scope("full_connect"):
+    W_o = tf.Variable(tf.random_normal([embedding_size, nclasses], stddev=0.01))
+    b_o = tf.Variable(tf.random_normal([nclasses], stddev=0.01))
 
-W_o = tf.Variable(tf.random_normal([embedding_size, nclasses], stddev=0.01))
-b_o = tf.Variable(tf.random_normal([nclasses], stddev=0.01))
+    outputs = tf.reshape(outputs, shape=[-1, embedding_size])
 
-outputs = tf.reshape(outputs, shape=[-1, embedding_size])
-outputs2 = tf.matmul(outputs, W_o) + b_o
+with tf.variable_scope("loss"):
+    all_outputs = tf.matmul(outputs, W_o) + b_o
+    all_answers = tf.reshape(input_y, shape=[-1, nclasses])
 
-y_answers = tf.split(1, max_sentence_length, input_y)
-y_answers2 = [tf.reshape(i, shape=[batch_size, -1]) for i in y_answers]
-y_answers2 = [tf.slice(answer, [0, 0], [sentence_length[i], nclasses]) for i, answer in enumerate(y_answers2)]
-
-all_outputs = tf.concat(0, outputs3)
-all_answers = tf.concat(0, y_answers2)
-
-losses = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(all_outputs, all_answers))
+    losses = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(all_outputs, all_answers))
 
 
 # In[16]:
@@ -197,7 +195,7 @@ test_data_gen = gen_test_data(batch_size)
 
 # In[ ]:
 
-X_test, y_test = next(test_data_gen)
+(X_test, y_testi, sl_test) = next(test_data_gen)
 
 
 # In[ ]:
@@ -233,6 +231,9 @@ with tf.Session() as sess:
                 sequence_length: sl
             }
 
+            print(X.shape)
+            print(y.shape)
+            print(sl)
             sess.run(optimizer, feed_dict=train_dict)   #perform an update on the parameters
 
             #create validation dictionary
@@ -244,7 +245,7 @@ with tf.Session() as sess:
             outputs, c_val = sess.run([all_outputs, losses], feed_dict=test_dict)            #compute the cost on the validation set
             print("Validation cost: {}, on Epoch {}".format(c_val, k))
 
-            if k >= num_epochs-1:
+            if k >= 0:
                 pred_1st = np.argmax(outputs[::max_sentence_length], axis=1)
                 pred_1st = [idx2label.get(i) for i in pred_1st]
 
@@ -256,6 +257,3 @@ with tf.Session() as sess:
                 for w, a, p in zip(word_1st, y_1st, pred_1st):
                     print(w.rjust(wlength), a.rjust(wlength), p.rjust(wlength))
                 print('\n'+'**'*30+'\n')
-
-
-# In[ ]:
